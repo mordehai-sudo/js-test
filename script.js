5e7ab8fced32833ad6e775f5fe7765fc8b3f0b14
Moti <mordehai@analiza-college.co.il>
2026-02-04 15:19:50 +0200

    first commit

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ let score = document.querySelector("#score");
 const h1 = document.getElementsByTagName('h1');
 const gameContainerEl = document.querySelector(".game-container");
 const errorMessage = document.querySelector('.error-message');
-// errorMessage.textContent = "Max score is required"
+const corseTitle = document.querySelector("#score-title");
+
+corseTitle.textContent = `The max score is:${score.value}`;
+
 
 
 formElement.addEventListener('submit', function (event) {
     event.preventDefault();
     if (event.target.score.value === "") {
-        // alert("Max score is required")
         errorMessage.textContent = "Max score is required"
     }
     console.log(event.target.score.value);
     localStorage.setItem(SCORE_TEY, event.target.score.value)
     window.location.href = "/game/game.html"
-
-
 });
 
-window.location.
+
 
 
 
